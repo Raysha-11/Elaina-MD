a9b593a7212d6d44f8a8ab8ce2c262a64f03e837
しんぞう <107793038+Shinz-07@users.noreply.github.com>
2022-06-30 13:54:24 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -599,6 +599,7 @@ export async function handler(chatUpdate) {
                 this.msgqueque.splice(quequeIndex, 1)
         }
         //console.log(global.db.data.users[m.sender])
+        await this.sendPresenceUpdtae('composing', m.chat)
         let user, stats = global.db.data.stats
         if (m) {
             if (m.sender && (user = global.db.data.users[m.sender])) {
