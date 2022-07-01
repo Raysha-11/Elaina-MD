90931f5c56018f7f78e2f996b77cd46177684126
しんぞう <107793038+Shinz-07@users.noreply.github.com>
2022-07-02 06:17:35 +0700

    Update downloader-mediafire.js

--- plugins/downloader-mediafire.js
+++ plugins/downloader-mediafire.js
@@ -3,13 +3,13 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
     if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.mediafire.com/file/941xczxhn27qbby/GBWA_V12.25FF-By.SamMods-.apk/file`
     let res = await mediafiredl(args[0])
     let { url, url2, filename, ext, aploud, filesize, filesizeH } = res
-    let caption = `
-*💌 Name:* ${filename}
-*📊 Size:* ${filesizeH}
-*🗂️ Extension:* ${ext}
-*📨 Uploaded:* ${aploud}
-`.trim()
-    m.reply(caption)
+    conn.sendButtonDoc(m.chat,`*Mediafire Downloader ✨*
+_Name : ${filename}_
+_Size : ${filesizeH}_
+_Extension : ${ext}_
+_Uploaded : ${aploud}_
+
+_*File sedang dikirim...*_`, wm, 'Menu', '.menu', m, ephemeral)
     await conn.sendFile(m.chat, url, filename, '', m, null, { mimetype: ext, asDocument: true })
 }
 handler.help = ['mediafire'].map(v => v + ' <url>')
