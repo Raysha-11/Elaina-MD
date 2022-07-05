d0deb054fe90e3f354d52dee305604f332e982d4
しんぞう <107793038+Shinz-07@users.noreply.github.com>
2022-07-05 09:04:50 +0700

    Update info-speed.js

--- plugins/info-speed.js
+++ plugins/info-speed.js
@@ -77,7 +77,7 @@ ${cpus[0].model.trim()} (${cpu.speed} MHZ)\n${Object.keys(cpu.times).map(type =>
 
 _CPU Core(s) Usage (${cpus.length} Core CPU)_
 ${cpus.map((cpu, i) => `${i + 1}. ${cpu.model.trim()} (${cpu.speed} MHZ)\n${Object.keys(cpu.times).map(type => `- *${(type + '*').padEnd(6)}: ${(100 * cpu.times[type] / cpu.total).toFixed(2)}%`).join('\n')}`).join('\n\n')}` : ''}
-`,titlebot, thumbdoc, sgc, 'Join here', null,null, [['Owner','.owner'],['Sewabot','.sewa'],[null,null]], m)
+`,wm, imgloc.getRandom(), sgc, 'Join here', null,null, [['Owner','.owner'],['Sewabot','.sewa'],['Back to menu' ,'.menu']], m, {asLocation: true})
 }
 handler.help = ['statserver']
 handler.tags = ['info']
