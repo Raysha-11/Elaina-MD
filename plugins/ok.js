11544ed259f7b411abc51c56d55c428a9a743d68
しんぞう <107793038+Shinz-07@users.noreply.github.com>
2022-07-06 04:58:34 +0700

    Delete ok.js

--- plugins/ok.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-
-conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/f61a7c96f3f86b6c5214f.png', m, { packname: "sticker by", author: "Agungx" })
-}
-
-handler.customPrefix = /^(ok)$/i
-handler.command = /^(ok)$/i
-
-module.exports = handler
