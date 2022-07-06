8401eb5f4c282a2ea7768dd56818a3896a05d015
Raysha-11 <108612413+Raysha-11@users.noreply.github.com>
2022-07-06 12:32:53 +0700

    Update cmd-list.js

--- plugins/cmd-list.js
+++ plugins/cmd-list.js
@@ -1,17 +1,17 @@
 let handler = async (m, { conn }) => {
     conn.reply(m.chat, `
-*DAFTAR HASH*
+*List daftar cmd*
 \`\`\`
 ${Object.entries(global.db.data.sticker).map(([key, value], index) => `${index + 1}. ${value.locked ? `(Terkunci) ${key}` : key} : ${value.text}`).join('\n')}
 \`\`\`
 `.trim(), null, {
         mentions: Object.values(global.db.data.sticker).map(x => x.mentionedJid).reduce((a, b) => [...a, ...b], [])
-    })
+    }, fcon)
 }
 
 
 handler.help = ['listcmd']
-handler.tags = ['database']
+handler.tags = ['database', 'premium']
 handler.command = ['listcmd']
 
 export default handler
