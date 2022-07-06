dc98940766b4ecea3a83e5978a91676ee9ab1fd6
Raysha-11 <108612413+Raysha-11@users.noreply.github.com>
2022-07-06 15:14:19 +0700

    Create anime-kotori.js

---
+++ plugins/anime-kotori.js
@@ -0,0 +1,13 @@
+import fetch from 'node-fetch'
+
+let handler = async (m, { conn, command }) => {
+	let url = 'https://revita.herokuapp.com/api/wallpaper/kotori?apikey=ApiRevita'
+        let kotori = '• Tap next\nuntuk menampilkan image selanjutnya'
+	conn.sendButton(m.chat, '*Nih kak >//<*\n_*Join gc bot di atas link nya*_', kotori, await(await fetch(url)).buffer(), [[' Next ',`.${command}`]],m, fakeig)
+}
+handler.command = /^(kotori)$/i
+handler.tags = ['anime']
+handler.help = ['kotori']
+handler.premium = false
+
+export default handler
