f23ed151deeb5726d69491bde9a987ba43dc77fb
Raysha-11 <108612413+Raysha-11@users.noreply.github.com>
2022-07-06 16:01:57 +0700

    Create anime-elaina.js

---
+++ plugins/anime-elaina.js
@@ -0,0 +1,13 @@
+import fetch from 'node-fetch'
+
+let handler = async (m, { conn, command }) => {
+	let url = 'https://revita.herokuapp.com/api/wallpaper/elaina?apikey=ApiRevita'
+        let elaina = '• Tap next\nuntuk menampilkan image selanjutnya'
+	conn.sendButton(m.chat, '*Nih kak >//<*\n_*Join gc bot di atas link nya*_', elaina, await(await fetch(url)).buffer(), [[' Next ',`.${command}`]],m, fakeig)
+}
+handler.command = /^(elaina)$/i
+handler.tags = ['anime']
+handler.help = ['elaina']
+handler.premium = false
+
+export default handler
