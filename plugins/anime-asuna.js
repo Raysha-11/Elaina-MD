47db25299c58225a330f8ad52dc55bb4d9c54994
Raysha-11 <108612413+Raysha-11@users.noreply.github.com>
2022-07-06 15:10:01 +0700

    Update anime-asuna.js

--- plugins/anime-asuna.js
+++ plugins/anime-asuna.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch'
 let handler = async (m, { conn, command }) => {
 	let url = 'https://api-reysekha.herokuapp.com/api/wallpaper/asuna?apikey=apirey'
         let asuna = '• Tap next\nuntuk menampilkan image selanjutnya'
-	conn.sendButton(m.chat, '*Nih wibukk*\n_*Join gc bot di atas link nya*_', asuna, await(await fetch(url)).buffer(), [[' Next ', `.${command}`]],m, fakeig)
+	conn.sendButton(m.chat, '*Nih kak >//<*\n_*Join gc bot di atas link nya*_', asuna, await(await fetch(url)).buffer(), [[' Next ', `.${command}`]],m, fakeig)
 }
 handler.command = /^(asuna)$/i
 handler.tags = ['anime']
