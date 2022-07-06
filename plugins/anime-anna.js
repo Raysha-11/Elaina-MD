14ec54e422b860ce98319981c60168e0e0df286d
Raysha-11 <108612413+Raysha-11@users.noreply.github.com>
2022-07-06 15:03:00 +0700

    Update anime-anna.js

--- plugins/anime-anna.js
+++ plugins/anime-anna.js
@@ -2,8 +2,8 @@ import fetch from 'node-fetch'
 
 let handler = async (m, { conn, command }) => {
 	let url = 'https://api.zeeoneofc.xyz/api/anime/anna?apikey=5Cd8U3tG'
-        let anna = '• Tap Animek\nuntuk menampilkan menu animek'
-	conn.sendButton(m.chat, '*Nih wibukk*\n_*Join gc bot di atas link nya*_', anna, await(await fetch(url)).buffer(), [['✧ Next', `.${command}`]],m, fakeig)
+        let anna = '• Tap next\nuntuk menampilkan image selanjutnya'
+	conn.sendButton(m.chat, '*Nih wibukk*\n_*Join gc bot di atas link nya*_', anna, await(await fetch(url)).buffer(), [[' Next ', `.${command}`]],m, fakeig)
 }
 handler.command = /^(anna)$/i
 handler.tags = ['anime']
