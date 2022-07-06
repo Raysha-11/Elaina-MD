c15c6c4e2a39024c9f5e26dc392bde8b478f978c
Raysha-11 <108612413+Raysha-11@users.noreply.github.com>
2022-07-06 15:11:11 +0700

    Update anime-kurumi.js

--- plugins/anime-kurumi.js
+++ plugins/anime-kurumi.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch'
 let handler = async (m, { conn, command }) => {
 	let url = 'https://revita.herokuapp.com/api/wallpaper/kurumi?apikey=ApiRevita'
         let kurumi = '• Tap next\nuntuk menampilkan image selanjutnya'
-	conn.sendButton(m.chat, '*Nih kak >//<*\n_*Join gc bot di atas link nya*_', kurumi, await(await fetch(url)).buffer(), [['Next',`.${command}`]],m, fakeig)
+	conn.sendButton(m.chat, '*Nih kak >//<*\n_*Join gc bot di atas link nya*_', kurumi, await(await fetch(url)).buffer(), [[' Next ',`.${command}`]],m, fakeig)
 }
 handler.command = /^(kurumi)$/i
 handler.tags = ['anime']
