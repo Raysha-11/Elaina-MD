4cbe4c9d0d390c8a80a8c2958a78cad2626db814
Raysha-11 <108612413+Raysha-11@users.noreply.github.com>
2022-07-06 14:54:13 +0700

    Update anime-mikasa.js

--- plugins/anime-mikasa.js
+++ plugins/anime-mikasa.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch'
 let handler = async (m, { conn, command }) => {
 	let url = 'https://revita.herokuapp.com/api/wallpaper/mikasa?apikey=ApiRevita'
         let mikasa = '• Tap Animek\nuntuk menampilkan menu anime'
-	conn.sendButton(m.chat, '\n*Nih wibukk*\n_*Join gc bot di atas link nya*_\n', mikasa, await(await fetch(url)).buffer(), [['Next',`.${command}`]],m, fakeig)
+	conn.sendButton(m.chat, '\n*Nih wibukk*\n_*Join gc bot di atas link nya*_\n', mikasa, await(await fetch(url)).buffer(), [['✨ Animek','.animek'], ['✧ Next', `.${command}`]],m, fakeig)
 }
 handler.command = /^(mikasa)$/i
 handler.tags = ['anime']
