7840443432468c6bad38751452c9ec5eca31b5bb
Raysha-11 <108612413+Raysha-11@users.noreply.github.com>
2022-07-07 05:33:51 +0700

    Update bank-nabung.js

--- plugins/bank-nabung.js
+++ plugins/bank-nabung.js
@@ -1,7 +1,10 @@
 const xpperlimit = 1
+import fs from 'fs'
+
 let handler = async (m, { conn, command, args }) => {
 	let user = global.db.data.users[m.sender]
   let count = command.replace(/^nabung/i, '')
+  let frep = { contextInfo: { externalAdReply: {title: global.wm, body: global.author, sourceUrl: sgh, thumbnail: fs.readFileSync('./thumbnail.jpg')}}}
   count = count ? /all/i.test(count) ? Math.floor(global.db.data.users[m.sender].money / xpperlimit) : parseInt(count) : args[0] ? parseInt(args[0]) : 1
   count = Math.max(1, count)
   if (user.atm == 0) return m.reply('kamu belum mempuyai atm !')
@@ -10,8 +13,8 @@ let handler = async (m, { conn, command, args }) => {
   if (global.db.data.users[m.sender].money >= xpperlimit * count) {
     global.db.data.users[m.sender].money -= xpperlimit * count
     global.db.data.users[m.sender].bank += count
-    conn.reply(m.chat, `Sukses menabung sebesar ${count} Money 💹`, m)
-  } else conn.reply(m.chat, `[❗] Uang anda tidak mencukupi untuk menabung ${count} money 💹`, m)
+    conn.reply(m.chat, `Sukses menabung sebesar ${count} Money 💹`, m, frep)
+  } else conn.reply(m.chat, `[❗] Uang anda tidak mencukupi untuk menabung ${count} money 💹`, m, frep)
 }
 handler.help = ['nabung <jumlah>']
 handler.tags = ['rpg']
