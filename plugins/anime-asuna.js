7729f7fd6c56469ef474f7d4cd2f58132f0b49c5
Raysha-11 <108612413+Raysha-11@users.noreply.github.com>
2022-07-06 14:53:25 +0700

    Update anime-asuna.js

--- plugins/anime-asuna.js
+++ plugins/anime-asuna.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch'
 let handler = async (m, { conn, command }) => {
 	let url = 'https://revita.herokuapp.com/api/wallpaper/asuna?apikey=ApiRevita'
         let asuna = '• Tap Animek\nuntuk menampilkan menu animek'
-	conn.sendButton(m.chat, '\n*Nih wibukk*\n_*Join gc bot di atas link nya*_\n', asuna, await(await fetch(url)).buffer(), [['✨ Animek','.animek'], ['✧ Next', `.${command}`]],m)
+	conn.sendButton(m.chat, '\n*Nih wibukk*\n_*Join gc bot di atas link nya*_\n', asuna, await(await fetch(url)).buffer(), [['✨ Animek','.animek'], ['✧ Next', `.${command}`]],m, fakeig)
 }
 handler.command = /^(asuna)$/i
 handler.tags = ['anime']
