dc5d7904dd87763fe55ca1b1b1447ae7984a9a9c
Raysha-11 <108612413+Raysha-11@users.noreply.github.com>
2022-07-07 04:07:57 +0700

    Update _antivirus.js

--- plugins/_antivirus.js
+++ plugins/_antivirus.js
@@ -1,16 +1,11 @@
-const isVirtex = /PLHIPS|๒|๑|ৡ|⃟|Đ.Δ.Μ/i // tambahin sendiri
+let fs = require('fs')
+let { MessageType } = require('@adiwajshing/baileys')
+let handler = m => m
 
-export async function before(m, { conn, isAdmin, isBotAdmin }) {
-    if (m.isBaileys && m.fromMe)
-        return !0
-    if (!m.isGroup) return !1
-    let chat = global.db.data.chats[m.chat]
-    let bot = global.db.data.settings[this.user.jid] || {}
-    const isAntiVirtex = isVirtex.exec(m.text)
-    
-    if (chat.antiVirtex && isAntiVirtex) {
-    
-    let regs = /(ผิดุท้เึางืผิดุท้เึางื)/i
+handler.all = async function (m, { isBlocked }) {
+    if (isBlocked) return
+
+   let regs = /(ผิดุท้เึางืผิดุท้เึางื)/i
    let isVertexThai = regs.exec(m.text)
    if (isVertexThai && !m.fromMe) {
    conn.groupParticipantsUpdate(m.chat, [m.sender], "remove")
@@ -28,30 +23,9 @@ export async function before(m, { conn, isAdmin, isBotAdmin }) {
    conn.groupParticipantsUpdate(m.chat, [m.sender], "remove")
    }
    
-    if (m.message && m.isBaileys && m.quoted && m.quoted.mtype === 'orderMessage' && !(m.quoted.token && m.quoted.orderId)) {
-            m.reply('Bug Troli Detected\n\n' + require('util').format(m.key))
-            await this.clearMessage(m.chat, m.key)
-            await this.modifyChat(m.chat, 'clear', {
-                includeStarred: false
-            }).catch(console.log)
-        }
-            if (!m.isBaileys && m.text.length > 384)
-            if (m.text && m.text.length >= 25000)
-            if (m.messageStubType === 68) { //Auto clear jika terdapat pesan yg tidak dapat dilihat oleh whatsapp web
-        let log = {
-            key: m.key,
-            content: m.msg,
-            sender: m.sender
-        }
-        await this.modifyChat(m.chat, 'clear', {
-            includeStarred: false
-        }).catch(console.log)
-    }
-        await conn.sendButton(m.chat, `*Font Aneh detect!*${isBotAdmin ? '' : '\n\n_Bot bukan admin_'}`, author, ['off antivirtex', '/disable antivirtex'], m)
-        if (isBotAdmin && bot.restrict) {
-       return m.reply('Ok!')
-        await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
-        } else if (!bot.restrict) return m.reply('Owner disable auto kick!')
-    }
-    return !0
+   // tambahin sendiri code virus WhatsApp..
 }
+
+handler.botAdmin = true
+handler.group = true
+export default handler
